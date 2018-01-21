90f3a1282ffc219cf2a3fbc585dbe3acf1dd094c
Brian Poole <brianpooledev@gmail.com>
2018-01-21 17:05:23 -0600

    update again

--- src/components/video_detail/video_modal.js
+++ src/components/video_detail/video_modal.js
@@ -3,24 +3,65 @@ import Modal from 'react-modal';
 
 import { connect } from 'react-redux';
 
-const VideoModal = (props) => (    
-    <Modal
-        isOpen={!!props.movieSelected} //!!props.movieSelected
-        contentLabel="Movie Detail"    
-    >
-    {props.detail && console.log(props.detail.Title)}  
-        <h3>Suspicio? Bene ... tunc ibimus? Quis uh ... CONEXUS locus his diebus? Quisque semper aliquid videtur, in volutpat mauris. Nolo enim dicere. Vobis neque ab aliis. Ego feci memetipsum explicans. Gus mortuus est. Lorem opus habeo. </h3>
-        <button onClick={props.handleCloseDetail}>Close Detail</button>
-  
-    </Modal>
-  );
+const VideoModal = props => (
+  <Modal
+    isOpen={!!props.movieSelected} //!!props.movieSelected
+    contentLabel="Movie Detail"
 
-  function mapStateToProps({ detail }) {
-      console.log();
-    //console.log(typeof movie);
-    return { detail };
-  }
-  
-  // REDUX EXPORT
+  >
+    {props.detail && console.log(props.detail.Title)}
+
+    <div className="row">
+      <div className="col-md-4">
+        <img src={props.detail.Poster} className="thumbnail" />
+        <h2>
+          {props.detail.Title} ({props.detail.Year})
+        </h2>
+      </div>
+      <div className="col-md-8">
+        <ul className="list-group">
+          <li className="list-group-item">
+            <strong>Released: </strong>
+            {props.detail.Released}
+          </li>
+          <li className="list-group-item">
+            <strong>Rated: </strong>
+            {props.detail.Rated}
+          </li>
+          <li className="list-group-item">
+            <strong>Runtime: </strong>
+            {props.detail.Runtime}
+          </li>
+          <li className="list-group-item">
+            <strong>Starring: </strong>
+            {props.detail.Actors}
+          </li>
+          <li className="list-group-item">
+            <strong>Director: </strong>
+            {props.detail.Director}
+          </li>
+          <li className="list-group-item">
+            <strong>Writer: </strong>
+            {props.detail.Writer}
+          </li>
+          <li className="list-group-item">
+            <strong>Plot: </strong>
+            {props.detail.Plot}
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <button onClick={props.handleCloseDetail}>Close Detail</button>
+  </Modal>
+);
+
+function mapStateToProps({ detail }) {
+  console.log();
+  //console.log(typeof movie);
+  return { detail };
+}
+
+// REDUX EXPORT
 
 export default connect(mapStateToProps)(VideoModal);
