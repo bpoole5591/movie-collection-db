00175a522ab1469ce55657d6b1098b6197aeafeb
Brian Poole <brianpooledev@gmail.com>
2018-01-21 17:53:13 -0600

    fixed bug

---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  AUTH_DOMAIN: 'brianspoole.auth0.com',
+  CLIENT_ID: 'HVCLJSULMA4gaPyfDgH-ujGY2u1ztG95',
+  CLIENT_SECRET:
+    'uOIE11sxTW1SRAj7LCnRq5vxkmqto9lY78llTCXbipn9k_AZpu_4nm1bZ4QT50FJ'
+};
