4ea4bacbfe64f616bb7b13c77ec293db5e213929
Brian Poole <brianpooledev@gmail.com>
2018-01-29 16:37:16 -0600

    fix key

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,8 +1,8 @@
 module.exports = {
-  googleClientID: '147086765619-h2gms53ueft3omdblloh84ddfb9dfgpn.apps.googleusercontent.com',
-  googleClientSecret: 'jd0kk_CcJXFptD0uMnnPZF_t',
+  googleClientID: '803657737220-kaoapihjpijkovs4cdudb3pd85205jpg.apps.googleusercontent.com',
+  googleClientSecret: 'GJgNLJxlNc5-nfeEc_lAS3mt',
   mongoURI: 'mongodb://brianpoole:password@ds111258.mlab.com:11258/themoviecollectiondatabase',
   cookieKey: 'thisisarandomstring',
   stripePublishableKey: 'pk_test_om9cJEx8t2HT7PgYOkjvoTZS',
-  stripeSecretKey: 'sk_test_1l2vde5LxBuZBr9RxJF1fIe4'
+  stripeSecretKey: 'sk_test_1l2vde5LxBuZBr9RxJF1fIe4',
 };
