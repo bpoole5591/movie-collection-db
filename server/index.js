bbfa96a85e87ebfdb550ced4b815050092f30a21
Brian Poole <brianpooledev@gmail.com>
2018-01-29 11:50:45 -0600

    fixed build error

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ mongoose.connect(config.mongoURI);
 
 const app = express();
 
-// app.use(express.static(`${__dirname}/../build`));
+app.use(express.static(`${__dirname}/../build`));
 app.use(json());
 
 massive(process.env.CONNECTION_STRING)
@@ -40,10 +40,10 @@ app.get('/api/collection', control.fetchCollection);
 app.delete('/api/collection/delmov/:googleID/:imdbID', control.deleteMovie);
 app.delete('/api/collection/delcol/:googleID', control.deleteCollection);
 
-// const path = require('path');
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../build/index.html'));
-// });
+const path = require('path');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 const PORT = process.env.PORT || 3005;
 
